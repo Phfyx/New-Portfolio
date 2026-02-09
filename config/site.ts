a7df07a1af5b16c7c840ab2f50798aef9cf7e568
mohamed <mohamedbenomar145@gmail.com>
2026-02-09 17:41:48 +0100

    FEAT: header bar

---
+++ config/site.ts
@@ -0,0 +1,39 @@
+export const siteConfig = {
+  name: 'mimo.dev',
+  url: 'https://aadi.is-a.dev',
+  description: 'Portfolio de Mohamed Benomar, développeur Junior et entousiaste DevOps',
+  keywords: [
+    'Mohamed Benomar',
+    'aditya domle',
+    'aditya.com',
+    'Aditya Domle Portfolio',
+    'Full Stack Developer',
+    'Web Developer',
+    'DevOps Engineer',
+    'Next.js Developer',
+    'React Developer',
+    'Node.js Developer',
+    'Aditya Domle Projects',
+    'Aditya Domle Skills',
+    'Aditya Domle Experience',
+    'Aditya Domle Education',
+    'Aditya Domle Contact',
+    'Aditya Domle Resume',
+  ],
+  links: {
+    shadcn: 'https://ui.shadcn.com/',
+    github: 'https://github.com/Phfyx',
+    githubProfile: 'https://github.com/Phfyx',
+    linkedin: 'https://www.linkedin.com/in/mohamedbenomar/',
+    resume: "https://drive.google.com/file/d/17XC0X3qFr5RUZ5hIgtDusv0IcY97KK_I/view?usp=drive_link", // Add your resume link here
+    email: 'mailto:mohamedbenomar145@gmail.com',
+    githubUsername: 'Phfyx',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b',
+};
